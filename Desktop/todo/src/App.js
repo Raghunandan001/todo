d9b791ec3f83b5b60a089f07dacfc06388499691
Raghunandan001 <raghunandanadabala@gmail.com>
2021-02-05 16:12:24 +0530

    updated

---
+++ Desktop/todo/src/App.js
@@ -0,0 +1,63 @@
+import React,{Component} from 'react';
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      formInputData:"",
+       storeData:[]
+    }
+  }
+  storeInputData=(event1)=>{
+    this.setState({
+      formInputData:event1.target.value
+    })
+  }
+  storeFinalData=()=>{
+    this.setState({
+      storeData:[...this.state.storeData,this.state.formInputData]
+    })
+  }
+  //Deleting an item from array
+  deleteStoreData=(event2)=>{
+    var tempData=this.state.storeData;
+    if(window.confirm("Are you sure ?")==true){
+      tempData.splice(Number(event2.target.id),1)
+      this.setState({
+        storeData:tempData
+      })
+    }
+
+  }
+  render(){
+    return(
+      <div className="row">
+        {/*First column*/}
+        <div className="col-lg-5 col-md-10">
+          <div className="card mt-8">
+            <div className="card-body">
+              <form>
+                <input type="text" className="form-control"  placeholder="Enter..."
+                onKeyUp={(InputEvent)=>{this.storeInputData(InputEvent)}}/><br />
+                <button type="button" class="btn btn-success" onClick={this.storeFinalData}> Add data </button>
+              </form>
+              </div>
+          </div>
+        </div>
+        {/*Second column*/}
+        <div className="col-lg-5 col-md-10">
+          <div className="card mt-8">
+            <div className="card-body">
+              <ul className="list-group">
+                {this.state.storeData.map((item,index)=>(
+                  <li className="list-group-item" key={index}> {item} <button className="btn btn-danger"id={index} 
+                  onClick={(deleteEvent)=>{this.deleteStoreData(deleteEvent)}} >Delete </button>
+                  </li>
+                ))}
+              </ul>
+              </div>
+            </div>
+            </div>
+            </div>
+    )
+  }
+}
